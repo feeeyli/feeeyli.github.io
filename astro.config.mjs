f8738185046701657e692dd703720e79cf027cf8
Luna Santos <lunafeyli@gmail.com>
2024-02-13 10:56:38 -0300

    Add deploy github worlflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
 
 export default defineConfig({
+  site: "https://feeeyli.github.io",
   integrations: [
     UnoCSS({
       injectReset: true,
